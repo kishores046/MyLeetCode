b5ae0424702abd3d7025cb11ea46ec61423439b7
kishores046 <kishore279k@gmail.com>
2024-12-22 23:42:37 +0530

    Add files via upload

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,37 @@
+
+ //Definition for a binary tree node.
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+       List<Integer> list=new ArrayList<>();
+       inorder(root,list);
+       return list;
+    }
+    public void inorder(TreeNode node,List<Integer> list){
+        if(node!=null){
+            inorder(node.left,list);
+            list.add(node.val);
+            inorder(node.right,list);
+        }
+    }
+    public static void main(String args[]){
+        TreeNode root=new TreeNode(1,null,new TreeNode(2,new TreeNode(3),null));
+        List<Integer> result=new BinaryTreeInorderTraversal().inorderTraversal(root);
+        System.out.println(result);
+    }}
