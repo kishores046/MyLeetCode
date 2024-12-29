e01285a29d4a7acb5e1798e4e7de4c5d6c1a3231
kishores046 <kishore279k@gmail.com>
2024-12-29 12:26:18 +0530

    Commit ExcelSheetColumnTitle

---
+++ ExcelSheetColumnTitle.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        
+        
+        int num=columnNumber;
+        StringBuilder s=new StringBuilder("");
+        while(num>0){
+            num--;
+            s.insert(0,(char)('A'+num%26));
+            num/=26;
+        }
+        return s.toString();
+    }
+}
