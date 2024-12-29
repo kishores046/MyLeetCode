4032916a2b91976fa3894ba855fb42ba6437d65c
kishores046 <kishore279k@gmail.com>
2024-12-29 13:10:50 +0530

    Commit MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,22 +1,11 @@
-package LEETCODE_JAVA.EASY;
-
-import java.util.Arrays;
-
-public class MissingNumber {
-    public int findMissingNumber(int arr[]){
-       int n=arr.length;
-       for(int i=0;i<=n;i++){
-        if(Arrays.binarySearch(arr,i)<0){
-            return i;
+import java.util.*;
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int expected_sum=n*(n+1)/2;
+        int actual_sum=0;
+        for(int i=0;i<nums.length;i++){
+            actual_sum+=nums[i];
         }
-       }
-        return 0;
-    }
-    public static void main(String[] args) {
-        int[] arr={3,0,1};
-        Arrays.sort(arr);
-        //System.out.println(Arrays.binarySearch(arr,3));
-        MissingNumber obj=new MissingNumber();
-       System.out.println(obj.findMissingNumber(arr));
-    }
-}
+        return expected_sum-actual_sum;
+    }}
