28bf2adabdd8e106a3861c374705849e545f7962
kishores046 <kishore279k@gmail.com>
2025-01-14 14:14:04 +0530

    Commit UniqueNumberOfOccurences.java

---
+++ UniqueNumberOfOccurences.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        HashSet<Integer> map1 = new HashSet<>();
+        for (int i:map.values()){
+            if (!map1.add(i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
