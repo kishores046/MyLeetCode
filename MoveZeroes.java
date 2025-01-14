bed2f5efbbc8b6e7ef981551da29450aecd093b2
kishores046 <kishore279k@gmail.com>
2025-01-14 14:15:45 +0530

    Commit MoveZeroes.java

---
+++ MoveZeroes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public void moveZeroes(int[] nums) {
+     int non_zero=0;
+     for(int i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            int temp=nums[non_zero];
+            nums[non_zero]=nums[i];
+            nums[i]=temp;
+            non_zero++;
+        }
+     } 
+    }
+}
