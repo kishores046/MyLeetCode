9f1a9dbb91ca33869ca9ce602d1de95ef09fa368
kishores046 <kishore279k@gmail.com>
2025-08-02 17:43:56 +0530

    Commit ValidParanthesis.java

---
+++ ValidParanthesis.java
@@ -0,0 +1,17 @@
+class ValidParanthesis {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(stack.isEmpty() || (c=='{' || c=='[' || c=='(')){
+                stack.push(c);
+            }
+            else if((stack.peek()=='{' && c=='}') || (stack.peek()=='(' && c==')') || (stack.peek()=='[' && c==']')){
+              stack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
