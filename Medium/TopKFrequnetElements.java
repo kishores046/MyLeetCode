cd249c166b6e3251364b6e5dac66aefe2967cacf
kishores046 <kishore279k@gmail.com>
2025-09-05 00:26:41 +0530

    Commit TopKFrequnetElements.java

---
+++ Medium/TopKFrequnetElements.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums.length==k){
+            return nums;
+        }
+        
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int iterable_element : nums) {
+            map.put(iterable_element,map.getOrDefault(iterable_element,0)+1);
+        }
+       PriorityQueue<Integer> minHeap=new PriorityQueue<>(Comparator.comparingInt(map::get));
+       for(int key:map.keySet()){
+        minHeap.add(key);
+        if(minHeap.size()>k){
+            minHeap.poll();
+        }
+       }
+        int[] result=new int[k];
+        for(int i=k-1;i>=0;i--){
+            result[i]=minHeap.poll();
+        }
+        return result;
+        
+    }
+}
