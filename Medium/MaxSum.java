b469419def0a8b326f3015a0ac18efbc9a676507
kishores046 <kishore279k@gmail.com>
2025-11-08 14:48:40 +0530

    Commit Medium\MaxSum

---
+++ Medium/MaxSum.java
@@ -0,0 +1,21 @@
+public class MaxSum {
+        public int maxSubArray(int[] nums) {
+            int maxSum = Integer.MIN_VALUE;
+            int currentSum = 0;
+
+            for (int num : nums) {
+                currentSum += num;
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+
+                if (currentSum < 0) {
+                    currentSum = 0;
+                }
+            }
+
+            return maxSum;
+        }
+    }
+
