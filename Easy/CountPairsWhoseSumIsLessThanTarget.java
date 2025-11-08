9ab11dff7b7830082163022a1157ea977bf48aa2
kishores046 <kishore279k@gmail.com>
2025-11-08 21:20:33 +0530

    Commit Easy\CountPairsWhoseSumIsLessThanTarget

--- Easy/CountPairsWhoseSumIsLessThanTarget.java
+++ Easy/CountPairsWhoseSumIsLessThanTarget.java
@@ -3,11 +3,11 @@ import java.util.List;
 
 public class CountPairsWhoseSumIsLessThanTarget {
     static void main() {
-        List<Integer> nums= Arrays.asList(1,2,3,4,5);
+        List<Integer> nums= Arrays.asList(-6,2,5,-2,-7,-1,3);
         nums.sort(Integer::compareTo);
-
+        System.out.println(nums);
         int l=0,h=nums.size()-1;
-        int target=7;
+        int target=-2;
         int count=0;
         while(l<h){
             int mid=nums.get(l)+ nums.get(h);
