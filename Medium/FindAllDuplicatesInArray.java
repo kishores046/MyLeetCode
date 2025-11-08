d2d865e30904fd96e81f021f77324d4a925af859
kishores046 <kishore279k@gmail.com>
2025-11-08 14:51:57 +0530

    Commit Medium\FindAllDuplicatesInArray

---
+++ Medium/FindAllDuplicatesInArray.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicatesInArray {
+
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                res.add(index + 1);
+            }
+            nums[index] = nums[index] * -1;
+        }
+        return res;
+    }
+
+}
