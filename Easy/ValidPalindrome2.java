8845934482c179feb311fa482b52e291d4e36905
kishores046 <kishore279k@gmail.com>
2026-02-16 21:16:41 +0530

    Commit: easy/ValidPalindrome.java

---
+++ Easy/ValidPalindrome2.java
@@ -0,0 +1,32 @@
+public class ValidPalindrome2 {
+    static void main() {
+        String str="aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+        System.out.println(validPalindrome(str));
+    }
+    public static boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return isPalindrome(s, i + 1, j)
+                        || isPalindrome(s, i, j - 1);
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
