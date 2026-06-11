64c4944da3eed6fe88bfa72329572977fa686071
kishore_s <kishore279k@gmail.com>
2026-06-11 19:02:16 +0530

    Added Dutch National Algorithm for Sort the Colors LC:75

--- Medium/SortColors.java
+++ Medium/SortColors.java
@@ -1,19 +1,21 @@
 public class SortColors {
     public void sortColors(int[] nums) {
-        int max=Integer.MIN_VALUE;
-    for(int i:nums){
-        max=Math.max(i,max);
-    }
-    int[] count=new int[max+1];
-    for(int i=0;i<nums.length;i++){
-        count[nums[i]]++;
-    }
-    int index=0;
-    for(int i=0;i<count.length;i++){
-        while(count[i]>0){
-            nums[index++]=i;
-            count[i]--;
+        int max = Integer.MIN_VALUE;
+        for (int i : nums) {
+            max = Math.max(i, max);
+        }
+        int[] count = new int[max + 1];
+        for (int num : nums) {
+            count[num]++;
+        }
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                nums[index++] = i;
+                count[i]--;
+            }
         }
     }
-    }
 }
+
+
